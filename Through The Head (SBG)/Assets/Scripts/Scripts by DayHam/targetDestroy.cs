3d6a8aaaaf6fea3ec97090f0b180546353ee9fdf
Day-Ham <37504037+Day-Ham@users.noreply.github.com>
2018-04-22 21:59:21 +0800

    Added My progress

---
+++ Through The Head (SBG)/Assets/Scripts/Scripts by DayHam/targetDestroy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetDestroy : MonoBehaviour {
+	public float health = 50f;
+	public bool armorOn= false;
+	public float armorUses;
+
+
+	public void TakeDamage(float amount){
+
+
+		if(armorOn){
+
+			armorUses -= amount;
+			if (armorUses <= 0f) {
+				health -= amount;
+				return;
+			}
+
+			health -= amount - 5;
+			return;
+
+		}else{
+			health -= amount;
+		if (health <= 0f) {
+				Die ();
+			} 
+
+	}
+}
+
+	void Die(){
+		
+		Destroy (gameObject);
+	}
+}
